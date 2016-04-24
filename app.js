9112d966b395b0195e04e68eb82516dbe894382a
Ben Moore <btmoore95@gmail.com>
2016-04-24 14:05:54 -0500

    added Files

---
+++ app.js
@@ -0,0 +1,27 @@
+
+function myFunction() {
+	fname = document.getElementById('fname').value; 
+	lname = document.getElementById('lname').value;
+	pnum = document.getElementById('pnum').value;
+	email = document.getElementById('email').value;
+	hschool = document.getElementById('hschool').value;
+	grade = document.getElementById('grade').value;
+	subject = document.getElementById('subject').value; 
+	other = document.getElementById('other').value; 
+
+	if ((fname=="") || (lname=="") || (pnum=="") || (email=="")){
+		document.getElementById("p1").innerHTML = "Please fill in the required fields.";
+		return; 
+	}
+
+	document.getElementById("p1").innerHTML = "Thanks for Sending a Contact Form";
+	$.ajax({
+		url: "https://formspree.io/btmoore95@gmail.com",
+		method: 'POST', 
+		data: {message: "hello!"},
+		dataType: "json"
+	}).done(function(response) {
+		console.log(response); 
+	}); 
+}
+
